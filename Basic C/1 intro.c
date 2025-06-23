628a7a3af2dc62684bd634c873d78a24fbed26e8
Sojwal Patil <sojwalpatil2006@gmail.com>
2025-06-23 14:15:29 +0530

    removed unnecessry things

--- Basic C/1 intro.c
+++
@@ -1,6 +0,0 @@
-// Write a C program to print "Hello, World!" using proper structure (#include, main, return).
-#include <stdio.h>
-int main(){
-    printf("Hello, World!\n");
-    return 0;
-}
--- Basic C/3 Data Type .c
+++
@@ -1,4 +0,0 @@
-// Declare variables of basic data types (int, float, char, double) and print them.
-// Define an enum for days of the week and print one value from it.
-// Demonstrate implicit type conversion between int and float.
-// Demonstrate explicit (manual) type casting from float to int.
